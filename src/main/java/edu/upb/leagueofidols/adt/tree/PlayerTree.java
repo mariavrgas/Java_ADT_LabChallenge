c72cbaadf99018c543bc6b583e455e598fef46c4
LuxyKolink <69657804+LuxyKolink@users.noreply.github.com>
2021-10-18 19:32:11 -0500

    Add() method
    Confirmo que aprendimos a hacer forks

--- src/main/java/edu/upb/leagueofidols/adt/tree/PlayerTree.java
+++ src/main/java/edu/upb/leagueofidols/adt/tree/PlayerTree.java
@@ -17,8 +17,41 @@ public class PlayerTree implements ITree<Player>{
 
     @Override
     public void add(Player object) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        root = addRecursive(object, root);
     }
+    
+    public TreeNode<Player> addRecursive(Player object, TreeNode<Player> current){
+        
+        if (current == null){
+            return new TreeNode<>(object);
+        }
+        
+        //    POSIBLE ERROR AQUÍ
+        int val = object.compareTo(current.getValue());
+        
+        /*
+        if (val == -1){
+        current.setLeft(addRecursive(object, current.getLeft()));
+        } else if (val == 0){
+        current.setRight(addRecursive(object, current.getRight()));
+        } else {
+        System.out.println("¡El valor ya existe!");
+        return current;
+        }
+         */
+        switch (val) {
+            case -1 -> current.setLeft(addRecursive(object, current.getLeft()));
+            case 0 -> current.setRight(addRecursive(object, current.getRight()));
+            default -> {
+                System.out.println("¡El valor ya existe!");
+                return current;
+            }
+        }
+        
+        return current;
+        
+    }
+    
 
     @Override
     public void find(Player object) {
